4277580c29d4021379fda85beb2357fa36a68d3d
KZ <zengke940420@gmail.com>
2024-06-20 02:35:53 -0400

    calc and plot

---
+++ R/CalculationTools.R
@@ -0,0 +1,58 @@
+
+#' Function to normalize counts table
+normFun <- function(table) {
+  n<-colSums(table)
+  sumx<-sum(table)
+  for (j in 1:ncol(table)) {
+    table[,j]<-table[,j]/n[j]
+  }
+  table<-log10(table*(sumx/ncol(table))+1)
+  table<-data.frame(table)
+  return(table)
+}
+
+#' Function to calculate t-test results
+calcTtest <- function(table, meta) {
+  t_stats<-apply(table, 1, function(x){t.test(unlist(x)~meta$conditions)$stat})
+  t_test_p<-apply(table, 1, function(x){t.test(unlist(x)~meta$conditions)$p.value})
+
+  t_results<-cbind(t_stats, t_test_p)
+  rownames(t_results)<-rownames(table)
+  colnames(t_results)<-c("stats", "pval")
+  t_results<-data.frame(t_results)
+  return(t_results)
+}
+
+#' Function to calculate DESeq2 results
+calcDESeq2 <- function(table, meta) {
+  #solve deseq2 all 0 issue
+  table<-table+1
+
+  meta$conditions<-factor(meta$conditions)
+  dds1 <- DESeqDataSetFromMatrix(countData=table,
+                                 colData=meta,
+                                 design=~conditions)
+  dds2 <- DESeq(dds1)
+  res <- results(dds2, cooksCutoff=FALSE, independentFiltering=FALSE)
+  deseq_results<-cbind(res$stat, res$pvalue)
+
+  rownames(deseq_results)<-rownames(table)
+  colnames(deseq_results)<-c("stats", "pval")
+  deseq_results<-data.frame(deseq_results)
+  return(deseq_results)
+}
+
+#' Function to generate Log10 p-values and assign test statistics directions
+directPFun<-function(ttest, deseq){
+  ttest$direction<-ttest$stats/abs(ttest$stats)
+  ttest$logP<-log10(ttest$pval)
+  ttest$p_directed<-ttest$direction*ttest$logP
+  deseq$direction<-deseq$stats/abs(deseq$stats)
+  deseq$logP<-log10(deseq$pval)
+  deseq$p_directed<-deseq$direction*deseq$logP
+  deseq$p_directed<-deseq$p_directed*-1
+  directP<-cbind(ttest$p_directed, deseq$p_directed)
+  directP<-data.frame(directP)
+  # directP<-directP[!apply(directP, 1, function(row) any(is.infinite(row))), ]
+  return(directP)
+}
---
+++ R/PlotTools.R
@@ -0,0 +1,100 @@
+library(ggplot2)
+
+#' Function to assign directions to log10 pvalues of t-test
+processTtestRes <- function(t_results) {
+  #creating log10 pvalues for t-test results
+  t_results$direction<-t_results$stats/abs(t_results$stats)
+  #log10 pvals
+  t_results$logP<-log10(t_results$pval)
+  #pval with direction
+  t_results$p_directed<-t_results$direction*t_results$logP
+
+  return(t_results)
+}
+
+#' Function to assign directions to log10 pvalues of DESeq2
+processDESeqRes <- function(deseq_results) {
+  #creating log10 pvalues for DESeq2 results
+  deseq_results$direction<-deseq_results$stats/abs(deseq_results$stats)
+  #log10 pvals
+  deseq_results$logP<-log10(deseq_results$pval)
+  #pval with direction
+  deseq_results$p_directed<-deseq_results$direction*deseq_results$logP
+  deseq_results$p_directed<-deseq_results$p_directed*-1
+
+  # for deseq2 0 p-value outputs
+  maxPosIndx<-which(deseq_results$direction>0 & deseq_results$pval == 0)
+  minNegIndx<-which(deseq_results$direction<0 & deseq_results$pval == 0)
+
+  maxPosP<-max(deseq_results$p_directed[deseq_results$p_directed < Inf])
+  minNegP<-min(deseq_results$p_directed[deseq_results$p_directed > -Inf])
+
+  deseq_results$p_directed[maxPosIndx]<-maxPosP + (maxPosP * 1e-6)
+  deseq_results$p_directed[minNegIndx]<-minNegP + (minNegP * 1e-6)
+
+  return(deseq_results)
+}
+
+#' Function to assign color code to different algorithm combo
+get_combination <- function(method1, method2) {
+  # Create a sorted vector to ignore the order of input methods
+  methods <- sort(c(method1, method2))
+
+  # Define all possible combinations and their respective results
+  combinations <- list(
+    `DESeq2 t-test` = c("turquoise1", "purple", "green", "red"),
+    `t-test Wilcoxon` = c("turquoise1", "green", "red", "tan2"),
+    `edgeR t-test` = c("turquoise1", "cornflowerblue", "green", "red"),
+    `DESeq2 Wilcoxon` = c("turquoise1", "purple", "green", "tan2"),
+    `DESeq2 edgeR` = c("turquoise1", "purple", "cornflowerblue", "green"),
+    `edgeR Wilcoxon` = c("turquoise1", "cornflowerblue", "green", "tan2")
+  )
+
+  # Check for the combination and return the result
+  for (combo in names(combinations)) {
+    if (all(methods == unlist(strsplit(combo, " ")))) {
+      return(combinations[[combo]])
+    }
+  }
+
+  return("Invalid combination")
+}
+
+#' Function to plot Log10 p-values
+Log10PvPPlot <- function(results1, results2, method1, method2, title = "Default title") {
+  # Plotting log10 p-values with directions based on p-value significance by which algorithm
+  df <- data.frame(results1$pval, results1$p_directed, results2$pval, results2$p_directed)
+  rownames(df) <- rownames(results1)
+  colnames(df) <- c("results1p", "results1Dp", "results2p", "results2Dp")
+
+  df <- df %>%
+    mutate(signif = case_when(
+      results1p < 0.05 & results2p < 0.05 ~ "Both",
+      results1p < 0.05 & results2p >= 0.05 ~ method1,
+      results1p >= 0.05 & results2p < 0.05 ~ method2,
+      TRUE ~ "Neither"
+    ))
+
+  counts <- df %>%
+    group_by(signif) %>%
+    summarise(count = n()) %>%
+    mutate(label = paste(signif, " (n=", count, ")", sep = ""))
+
+  df <- merge(df, counts, by = "signif")
+
+  color_combo<-get_combination(method1, method2)
+
+  p <- ggplot(df, aes(x = results1Dp, y = results2Dp, color = signif)) +
+    geom_point(alpha = 0.3) +
+    geom_abline(intercept = 0, slope = 1, col = "gray") +
+    scale_color_manual(values = color_combo,
+                       labels = counts$label) +
+    labs(color = "Significant by") +
+    theme_classic() +
+    ggtitle(title) +
+    xlab(paste0("log10(", method1, " p-value)")) +
+    ylab(paste0("log10(", method2, " p-value)"))
+
+  return(p)
+}
+
