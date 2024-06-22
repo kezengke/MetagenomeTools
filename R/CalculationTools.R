92708b2f3e73704a271255101eafb70263ac800f
KZ <zengke940420@gmail.com>
2024-06-22 14:03:48 -0400

    add rnorm resampling.

--- R/CalculationTools.R
+++ R/CalculationTools.R
@@ -86,3 +86,56 @@ directPFun<-function(ttest, deseq){
   # directP<-directP[!apply(directP, 1, function(row) any(is.infinite(row))), ]
   return(directP)
 }
+
+#' Function to resample counts table with multiple (Rnorm)
+resampleRNORM <- function(table, meta, multiple) {
+  if (nrow(table) == 0 || nrow(meta) == 0) {
+    stop("Input table or meta data frame is empty.")
+  }
+
+  groups <- unique(meta$conditions)
+  if (length(groups) != 2) {
+    stop("The function currently supports exactly two groups.")
+  }
+
+  group1 <- groups[1]
+  group2 <- groups[2]
+
+  # Function to calculate mean and sd for each group
+  calculateMeanSd <- function(z) {
+    g1 <- unlist(z[meta$conditions == group1])
+    g2 <- unlist(z[meta$conditions == group2])
+    c(mean1 = mean(g1), mean2 = mean(g2), sd1 = sd(g1), sd2 = sd(g2))
+  }
+
+  # Apply the function to each row of the table and combine results into a data frame
+  MeanSd_table <- t(apply(table, 1, calculateMeanSd))
+  MeanSd_table <- as.data.frame(MeanSd_table)
+  rownames(MeanSd_table) <- rownames(table)
+
+  # Function to generate resampled counts for each row
+  resample_counts <- function(row_index) {
+    z <- table[row_index,]
+    g1 <- unlist(z[meta$conditions == group1])
+    g2 <- unlist(z[meta$conditions == group2])
+
+    ng1 <- rnorm(n = length(g1), mean = MeanSd_table[row_index, "mean1"], sd = sqrt(multiple * (MeanSd_table[row_index, "sd1"])^2))
+    ng2 <- rnorm(n = length(g2), mean = MeanSd_table[row_index, "mean2"], sd = sqrt(multiple * (MeanSd_table[row_index, "sd2"])^2))
+
+    c(ng1, ng2)
+  }
+
+  # Apply the resampling function to each row
+  newT <- t(sapply(seq_len(nrow(table)), resample_counts))
+
+  # Set column and row names
+  colnames(newT) <- c(rownames(meta)[meta$conditions == group1], rownames(meta)[meta$conditions == group2])
+  newT <- newT[, colnames(table)]
+  rownames(newT) <- rownames(table)
+
+  # Replace negative values with 0 and round to integer
+  newT[newT < 0] <- 0
+  newT <- data.frame(round(newT, digits = 0))
+
+  return(newT)
+}
